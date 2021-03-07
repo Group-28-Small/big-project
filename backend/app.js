8adeabf8beb4b09d694b83a1fa98a3f78e02d981
Kurt Wilson <kurt4wilson@gmail.com>
2021-03-06 21:20:54 -0500

    (not yet tested or working) prep for local test and heroku deploy

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+const path = require('path');           
+const PORT = process.env.PORT || 5000;
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -12,9 +14,25 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', (process.env.PORT || 5000));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.set('view engine', 'html');
+// TODO: wrap this block in a PROD check
+app.listen(PORT, () => 
+{
+  console.log(`Server listening on port ${PORT}.`);
+});
 
+///////////////////////////////////////////////////
+// For Heroku deployment
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'frontend', 'big-project-web', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'big-project-web', 'build', 'index.html'));
+    });
+}
 module.exports = app;
---
+++ frontend/big-project-web/src/Common.js
@@ -0,0 +1,14 @@
+var app_name = "OUR_APP_HERE";
+function buildPath(route)
+{
+    if (process.env.NODE_ENV === 'production') 
+    {
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
+    }
+    else
+    {        
+        return 'http://localhost:5000/' + route;
+    }
+}
+
+export default buildPath;
