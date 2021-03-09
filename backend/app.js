b225c2907de29ad08dc8a71be006213a2c7c2027
Kurt Wilson <kurt4wilson@gmail.com>
2021-03-08 20:00:29 -0500

    add start server script. add nodemon to backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,5 @@
-const path = require('path');           
-const PORT = process.env.PORT || 5000;
+var path = require('path');           
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -14,16 +12,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('port', (process.env.PORT || 5000));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // app.set('view engine', 'html');
-// TODO: wrap this block in a PROD check
-app.listen(PORT, () => 
-{
-  console.log(`Server listening on port ${PORT}.`);
-});
 
 ///////////////////////////////////////////////////
 // For Heroku deployment
@@ -34,5 +26,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'frontend', 'big-project-web', 'build', 'index.html'));
     });
+} else {
+    // do nothing, because react server will run on it's own
 }
 module.exports = app;
---
+++ start_server.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+control_c()
+{
+  echo "Killing Scripts $backend_id $frontend_id"
+  kill -TERM -$backend_id
+  kill -TERM -$frontend_id
+  echo "done"
+  exit 0
+}
+echo "Setting control-c trap"
+trap control_c SIGINT
+setsid sh -c "cd backend ; export PORT=4000 ; FORCE_COLOR=true npm start |& cat | sed  's/^/[backend] /' " &
+backend_id=$!
+echo "Backend PID: $backend_id"
+# fix clearing screen issue
+setsid sh -c "cd frontend/big-project-web ; export PORT=3000 ; FORCE_COLOR=true npm start |& cat | sed  's/^/[frontend] /' " &
+frontend_id=$!
+echo "Frontend PID: $frontend_id"
+
+
+echo "done. waiting for SIGINT..."
+wait
+
