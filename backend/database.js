6f941544fc716cbfc740465dc83530237a9dc32e
Kurt Wilson <kurt4wilson@gmail.com>
2021-03-11 17:42:46 -0500

    riiiiggggghttttt, atob is browser-only

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,7 @@ const common_lib = require("big-project-common");
 var admin = require("firebase-admin");
 if (common_lib.is_production()) {
     admin.initializeApp({
-        credential: admin.credential.cert(JSON.parse(atob(process.env.FIREBASE_KEY_BASE64))),
+        credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.FIREBASE_KEY_BASE64, "base64"))),
     });
 } else {
     var serviceAccount = require("./cop4331-group21-bigproject-firebase-adminsdk-aepd8-9da7bc02cb.json");
