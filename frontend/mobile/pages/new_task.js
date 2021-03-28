baf213824d80b2be1c70c581244509d8bb353371
Kurt Wilson <kurt4wilson@gmail.com>
2021-03-27 22:53:19 -0400

    mobile: fix task submission, and clarify variable names

--- frontend/mobile/pages/new_task.js
+++ frontend/mobile/pages/new_task.js
@@ -13,19 +13,18 @@ export const NewTaskPage = props => {
     const { data: user } = useUser();
     const userDetailsRef = db.collection('users')
         .doc(user.uid);
-    const oniOS = Platform.OS == 'ios';
     const [timePickerVisible, setTimePickerVisible] = React.useState(false);
     const [timePickerMode, setTimePickerMode] = React.useState(false);
     const [hasDueDate, setHasDueDate] = React.useState(false);
     const [trackProgress, setTrackProgress] = React.useState(false);
 
-    const [name, onChangeName] = React.useState("");
-    const [time, onChangeTime] = React.useState("");
+    const [taskTime, onChangeName] = React.useState("");
+    const [estimatedTime, onChangeTime] = React.useState("");
     const [pct, onChangePct] = React.useState("0");
-    const [dueDate, setDueDate] = React.useState(new Date(Date.now() + (1000 * 60 * 60 * 24)));
+    const [dueDate, setDueDate] = React.useState(new Date(Date.now() + (1000 * 60 * 60 * 24))); // one day ahead
     const createTask = () => {
         console.log('Creating');
-        db.collection("tasks").doc().set({ 'name': name, 'user': userDetailsRef, 'time': time, 'percentage': pct, 'date': date.toLocaleString() });
+        db.collection("tasks").doc().set({ 'name': taskTime, 'user': userDetailsRef, 'estimated_time': estimatedTime, 'percentage': pct, 'due_date': dueDate });
         props.navigation.navigate('Home');
     }
     const showDatePicker = () => {
@@ -50,14 +49,14 @@ export const NewTaskPage = props => {
             <TextInput
                 style={styles.input}
                 onChangeText={onChangeName}
-                value={name}
+                value={taskTime}
                 label="Task Name"
                 mode="outlined"
             />
             <TextInput
                 style={styles.input}
                 onChangeText={onChangeTime}
-                value={time}
+                value={estimatedTime}
                 label="Expected Time (Optional)"
                 placeholder="hours:minutes"
                 keyboardType='numeric'
