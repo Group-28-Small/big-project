b262991954e1cd0de1a161271fb6be7e5c04588f
Alejandro Herrada <alejandroherradapons@gmail.com>
2021-04-02 00:42:20 -0400

    testing press and long press actions

--- frontend/mobile/pages/index.js
+++ frontend/mobile/pages/index.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { backend_address } from 'big-project-common';
 import AppStyles from '../styles';
 import { useFirestore, useFirestoreCollectionData, useUser } from 'reactfire';
@@ -22,13 +22,29 @@ export const IndexPage = props => {
     //     props.navigation.navigate('Edit Task');
     // }
 
+    // receive id from map for pulling relevant data (i.e. subtasks)? 
+    // or should the data be loaded already but change display from hidden to whatever
+    const handlerLongClick = () => {
+        alert("Long pressed :D");
+    }
+
+    const handlerClick = () => {
+        alert("Pressed :)");
+    }
+
     return (
         <View style={AppStyles.container}>
             <ScrollView>
             <Text>{backend_address("")}</Text>
             {tasks ? tasks.map((item) => {
                 return (
-                    <Text style={styles.tasks} key={item.id}>{item.name}{' \t'}{item.estimated_time}{' hrs\t'}{item.percentage}{'%\t'}{item.date}</Text>
+                    // trying out long press to edit
+                    <TouchableOpacity style={AppStyles.tasks}
+                        onPress={handlerClick}
+                        onLongPress={handlerLongClick}
+                        key={item.id}>
+                        <Text style={styles.tasks}>{item.name}{' \t'}{item.estimated_time}{' hrs\t'}{item.percentage}{'%\t'}{item.date}</Text>
+                    </TouchableOpacity>                    
                 );
             }) : <Text>No data</Text>}
             </ScrollView>
@@ -50,7 +66,9 @@ const styles = StyleSheet.create({
         color: 'black',
         fontSize: 15,
         textAlign: 'center',
-        margin: 2
-        
+        margin: 2,
+        paddingTop: 5,
+        paddingBottom: 5
     }
+
 });
