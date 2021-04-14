0e5307beacb60751fba96e70b57a5ccd2e46b3fa
Alex Rizk <Ace.K419@knights.ucf.edu>
2021-04-13 21:46:09 -0400

    new login and register page

--- frontend/big-project-web/src/components/Register.js
+++ frontend/big-project-web/src/components/Register.js
@@ -1,9 +1,10 @@
-import { Box, Button, Container, makeStyles, TextField } from '@material-ui/core';
+import { Box, Button, Container, makeStyles, TextField, Avatar, CssBaseline, FormControlLabel, Checkbox, Link, Grid, Typography } from '@material-ui/core';
 import { React, useState } from 'react';
 import { useAuth, useFirestore } from 'reactfire';
 import 'firebase/auth';
 import 'firebase/firestore';
 import { getOrCreateUserDocument } from 'big-project-common';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 export default function RegisterPage(props) {
     const styles = useStyles();
@@ -38,89 +39,67 @@ export default function RegisterPage(props) {
 
     }
 
+    // return (
+    //     <Container maxWidth="md">
+    //         <form>
+    //             <Box display='flex' justifyContent='center' alignContent='center' alignItems='center' flexDirection='column'>
+    //                 <TextField className={styles.field} required id='emailField' label='Email Address' type='email' variant='outlined' onChange={(event) => setEmail(event.target.value)} autoFocus={true}></TextField>
+    //                 <TextField className={styles.field} required id='emailField' type='password' label='Password' variant='outlined' onChange={(event) => setPassword(event.target.value)}></TextField>
+    //                 <Button variant='contained' color='primary' onClick={() => register()}>Register</Button>
+    //             </Box>
+    //         </form>
+    //     </Container>
+    // )
+
     return (
-//     <Container component="main" maxWidth="xs">
-//         <CssBaseline />
-//         <div className={classes.paper}>
-//         <Avatar className={classes.avatar}>
-//             <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//             Sign in
-//         </Typography>
-//         <form className={classes.form} noValidate>
-//             <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             id="email"
-//             label="Email Address"
-//             name="email"
-//             autoComplete="email"
-//             autoFocus
-//             />
-//             <TextField
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             name="password"
-//             label="Password"
-//             type="password"
-//             id="password"
-//             autoComplete="current-password"
-//             />
-//             <FormControlLabel
-//             control={<Checkbox value="remember" color="primary" />}
-//             label="Remember me"
-//             />
-//             <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//             >
-//             Sign In
-//             </Button>
-//             <Grid container>
-//             <Grid item xs>
-//                 <Link href="#" variant="body2">
-//                 Forgot password?
-//                 </Link>
-//             </Grid>
-//             <Grid item>
-//                 <Link href="#" variant="body2">
-//                 {"Don't have an account? Sign Up"}
-//                 </Link>
-//             </Grid>
-//             </Grid>
-//         </form>
-//         </div>
-//         <Box mt={8}>
-//         <Copyright />
-//         </Box>
-//     </Container>
-//   );
-        <Container maxWidth="md">
-            <form>
-                <Box display='flex' justifyContent='center' alignContent='center' alignItems='center' flexDirection='column'>
-                    <TextField className={styles.field} required id='emailField' label='Email Address' type='email' variant='outlined' onChange={(event) => setEmail(event.target.value)} autoFocus={true}></TextField>
-                    <TextField className={styles.field} required id='emailField' type='password' label='Password' variant='outlined' onChange={(event) => setPassword(event.target.value)}></TextField>
-                    <Button variant='contained' color='primary' onClick={() => register()}>Register</Button>
-                </Box>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={styles.paper}>
+            <Avatar className={styles.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Register
+            </Typography>
+            <form className={styles.form} noValidate>
+              <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus onChange={(event) => setEmail(event.target.value)} autoFocus={true}/>
+              <TextField variant="outlined" margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" onChange={(event) => setPassword(event.target.value)}/>
+              <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me"/>
+              <Button type="submit" fullWidth variant="contained" color="primary" className={styles.submit} onClick={() => register()}> Sign In </Button>
+              
             </form>
+          </div>
         </Container>
-    )
+    );
 }
 
 const useStyles = makeStyles((theme) => ({
-    field: {
-        margin: 8,
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
     },
-}));
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+
+// const useStyles = makeStyles((theme) => ({
+//     field: {
+//         margin: 8,
+//         '& .MuiTextField-root': {
+//             margin: theme.spacing(1),
+//             width: '25ch',
+//         },
+//     },
+// }));
