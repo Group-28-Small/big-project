8891c9c62eab4c60c1e93abeb01d5e978b08279e
Kurt Wilson <kurt4wilson@gmail.com>
2021-04-19 18:34:22 -0400

    web: test TaskTree

---
+++ frontend/big-project-web/src/components/TaskTree.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { render } from '@testing-library/react'
+import TaskTree from './TaskTree'
+import 'reactfire'
+import 'react-router'
+import 'react-router-dom'
+
+// we have to create dummy implementations of API calls
+const mockedUserResult = jest.fn()
+const mockedFirestoreDocData = jest.fn()
+const mockedFirestoreCollectionData = jest.fn()
+
+const mockedFirestoreResult = {
+    collection: (type) => {
+        if (type == 'tasks') {
+            return {
+                doc: () => ({ id: 1 }),
+                where: () => null,
+            }
+        } else {
+            return {
+                doc: () => 1
+            }
+        }
+    }
+}
+const mockedFirestoreCollectionDataResult = {
+    data: [{ id: '1' }, { id: '2' }]
+}
+const mockedFirestoreDocResult = {
+    data: { last_task_set_time: 0 }
+}
+
+jest.mock('reactfire', () => ({
+    useUser: () => ({
+        // we have to define any fields that the component uses
+        data: mockedUserResult()
+    }),
+    useFirestore: () => mockedFirestoreResult,
+    useFirestoreCollectionData: () => mockedFirestoreCollectionData(),
+    useFirestoreDocData: () => mockedFirestoreDocData(),
+    useAuth: () => 0
+}))
+jest.mock('react-router', () => ({
+    useHistory: () => ({
+        push: mockedHistoryPush
+    })
+}))
+jest.mock('react-router-dom', () => ({
+    Link: 'div'
+}))
+
+
+describe("Task Tree", () => {
+    it('renders without crashing', () => {
+        mockedFirestoreCollectionData.mockImplementation(() => mockedFirestoreCollectionDataResult)
+        mockedFirestoreDocData.mockImplementation(() => mockedFirestoreDocResult)
+        mockedUserResult.mockImplementation(() => 1)
+        render(<TaskTree />);
+        // check that it doesn't redirect
+    });
+});
