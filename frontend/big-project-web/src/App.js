5327a8e2d7270ab8206c4bb432d5239eb92f3015
Kurt Wilson <kurt4wilson@gmail.com>
2021-04-19 17:15:32 -0400

    web: cleanup imports in App.js

--- frontend/big-project-web/src/App.js
+++ frontend/big-project-web/src/App.js
@@ -1,15 +1,12 @@
-import { AppBar, Button, IconButton, makeStyles, Toolbar, Typography, createMuiTheme, ThemeProvider } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import { firebaseConfig, setAuthHandler, AppTheme } from 'big-project-common'
-import { useEffect, useState } from 'react';
-import { FirebaseAppProvider, useAuth, useFirebaseApp } from 'reactfire';
-import RouteController from './RouteController';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import moment from 'moment';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import MomentUtils from '@date-io/moment';
-import ErrorBoundary from './components/ErrorBoundary'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { AppTheme, firebaseConfig } from 'big-project-common';
+import { BrowserRouter } from 'react-router-dom';
+import { FirebaseAppProvider } from 'reactfire';
+import './App.css';
+import ErrorBoundary from './components/ErrorBoundary';
+import RouteController from './RouteController';
 
 const theme = createMuiTheme({
   palette: {
