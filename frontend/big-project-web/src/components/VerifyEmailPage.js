3b5beca45a7bc8543205926c0b810cada2ebede8
Kurt Wilson <kurt4wilson@gmail.com>
2021-04-23 23:03:01 -0400

    web: add some text to verify email

--- frontend/big-project-web/src/components/VerifyEmailPage.js
+++ frontend/big-project-web/src/components/VerifyEmailPage.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function VerifyEmailPage() {
     return (
-        <div>gyubdegwsyuo</div>
+        <>
+            <p>Check your email for a verification email!</p>
+            <p>Feel free to stay on this page. Once you verify your email, you will automatically be redirected to the home page.</p>
+        </>
+
     )
 }
